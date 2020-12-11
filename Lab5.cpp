c1e5e2f691ff9345fcb2836a11dad65c8be41c84
Nazar <milkysdave@gmail.com>
2020-12-11 11:44:05 +0200

    Update Lab5.cpp
    Fix 1.2

--- Lab5.cpp
+++ Lab5.cpp
@@ -6,83 +6,83 @@ using namespace std;
 
 
 class Musician {
-private:
-    string name;
-    int age;
-    double fee;
+    private:
+        string name;
+        int age;
+        double fee;
 
-public:
-    Musician() {};
-    Musician(string newName, double newFee, int newAge) {
-        this->name = newName;
-        this->fee = newFee;
-        this->age = newAge;
+    public:
+        Musician() {};
+        Musician(string newName, double newFee, int newAge) {
+            this->name = newName;
+            this->fee = newFee;
+            this->age = newAge;
+        };
+        ~Musician() {};
+
+        string getName() {
+            return name;
+        }
+        double getFee() {
+            return fee;
+        }
+        int getAge() {
+            return age;
+        }
     };
-    ~Musician() {};
-
-    string getName() {
-        return name;
-    }
-    double getFee() {
-        return fee;
-    }
-    int getAge() {
-        return age;
-    }
-};
 
 
 
 class MusicFestival {
-private:
-    double budget = 0;
-    vector <Musician> artists;
-    double currentBudget = 0;
+    private:
+        double budget = 0;
+        vector <Musician> artists;
+        double currentBudget = 0;
 
-public:
-    void addMusician(Musician a) {
-        if (a.getFee() + currentBudget < budget) {
-            artists.push_back(a);
-            currentBudget = currentBudget + a.getFee();
-            cout << "Artist " << a.getName() << " has been succesfully added to the list." << endl;
-        }
-        else {
-            cout << "We can not afford this artist, but we still have " << budget - currentBudget << " dollars." << endl;
-        }
-    }
-    void removeMusician(Musician a) {
-        artists.pop_back();
-        cout << "Artist " << a.getName() << " has been removed from the list." << endl;
-    }
-
-    vector <Musician> getArtists() {
-        return artists;
-    }
-    void setBudget(double maxBudget) {
-        budget = maxBudget;
-    }
-
-    MusicFestival() {
-    };
-    MusicFestival(vector<Musician> newArtists, double maxBudget) {
-        int i = 0;
-        budget = maxBudget;
-        double newArtistsBudget = 0;
-        for (i = 0; i < newArtists.size(); i++) {
-            newArtistsBudget = newArtistsBudget + newArtists.at(i).getFee();
-
-        }
-        if (newArtistsBudget > budget) {
-            cout << "You cant afford this artist." << endl;
-        }
-        else {
-            for (i = 0; i < newArtists.size(); i++) {
-                artists.push_back(newArtists.at(i));
+    public:
+        void addMusician(Musician a) {
+            if (a.getFee() + currentBudget < budget) {
+                artists.push_back(a);
+                currentBudget = currentBudget + a.getFee();
+                cout << "Artist " << a.getName() << " has been succesfully added to the list." << endl;
+            }
+            else {
+                cout << "We can not afford this artist, but we still have " << budget - currentBudget << " dollars." << endl;
             }
         }
-    }
-    ~MusicFestival() {};
-};
+        void removeMusician(Musician a) {
+            artists.pop_back();
+            cout << "Artist " << a.getName() << " has been removed from the list." << endl;
+        }
+
+        vector <Musician> getArtists() {
+            return artists;
+        }
+        void setBudget(double maxBudget) {
+            budget = maxBudget;
+        }
+
+        MusicFestival() {
+        };
+        MusicFestival(vector<Musician> newArtists, double maxBudget) {
+            int i = 0;
+            budget = maxBudget;
+            double newArtistsBudget = 0;
+            for (i = 0; i < newArtists.size(); i++) {
+                newArtistsBudget = newArtistsBudget + newArtists.at(i).getFee();
+
+            }
+            if (newArtistsBudget > budget) {
+                cout << "You cant afford this artist." << endl;
+            }
+            else {
+                for (i = 0; i < newArtists.size(); i++) {
+                    artists.push_back(newArtists.at(i));
+                }
+            }
+        }
+        ~MusicFestival() {};
+    };
 
 
 
