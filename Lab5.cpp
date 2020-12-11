2593db9df96176800cae2a8b045014449161867e
Nazar <milkysdave@gmail.com>
2020-12-11 11:29:44 +0200

    Update Lab5.cpp
    Changes were made reffering to MyroslavPro review.

--- Lab5.cpp
+++ Lab5.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 using namespace std;
 
+
+
 class Musician {
 private:
     string name;
@@ -11,17 +13,25 @@ private:
 
 public:
     Musician() {};
-    Musician(string new_name, double new_fee, int new_age) {
-        this->name = new_name;
-        this->fee = new_fee;
-        this->age = new_age;
+    Musician(string newName, double newFee, int newAge) {
+        this->name = newName;
+        this->fee = newFee;
+        this->age = newAge;
     };
     ~Musician() {};
 
-    string getName() { return name; }
-    double getFee() { return fee; }
-    int getAge() { return age; }
+    string getName() {
+        return name;
+    }
+    double getFee() {
+        return fee;
+    }
+    int getAge() {
+        return age;
+    }
 };
+
+
 
 class MusicFestival {
 private:
@@ -30,41 +40,44 @@ private:
     double currentBudget = 0;
 
 public:
-    void addMusician(Musician artist) {
-        if (artist.getFee() + currentBudget < budget) {
-            artists.push_back(artist);
-            currentBudget = currentBudget + artist.getFee();
-            cout << "Artist "<< artist.getName() <<" has been succesfully added to the list." << endl;
+    void addMusician(Musician a) {
+        if (a.getFee() + currentBudget < budget) {
+            artists.push_back(a);
+            currentBudget = currentBudget + a.getFee();
+            cout << "Artist " << a.getName() << " has been succesfully added to the list." << endl;
         }
         else {
-            cout << "You can not afford this artist, but you still have "<< budget - currentBudget << " dollars." << endl;
+            cout << "We can not afford this artist, but we still have " << budget - currentBudget << " dollars." << endl;
         }
     }
-    void removeMusician(Musician artist) {
+    void removeMusician(Musician a) {
         artists.pop_back();
-        cout << "Artist " << artist.getName() << " has been removed from the list." << endl;
+        cout << "Artist " << a.getName() << " has been removed from the list." << endl;
     }
 
-    vector <Musician> getArtists() { return artists; }
-    void SetBudget(double maxBudget) {
+    vector <Musician> getArtists() {
+        return artists;
+    }
+    void setBudget(double maxBudget) {
         budget = maxBudget;
     }
 
-    MusicFestival() {};
-    MusicFestival(vector<Musician> artist1, double maxBudget) {
-        int i = 0; 
+    MusicFestival() {
+    };
+    MusicFestival(vector<Musician> newArtists, double maxBudget) {
+        int i = 0;
         budget = maxBudget;
-        double artist1budget = 0;
-        for (i = 0; i < artist1.size(); i++) {
-            artist1budget = artist1budget + artist1.at(i).getFee();
-                
+        double newArtistsBudget = 0;
+        for (i = 0; i < newArtists.size(); i++) {
+            newArtistsBudget = newArtistsBudget + newArtists.at(i).getFee();
+
         }
-        if (artist1budget > budget) {
+        if (newArtistsBudget > budget) {
             cout << "You cant afford this artist." << endl;
         }
         else {
-            for (int i = 0; i < artist1.size(); i++) {
-                artists.push_back(artist1.at(i));
+            for (i = 0; i < newArtists.size(); i++) {
+                artists.push_back(newArtists.at(i));
             }
         }
     }
@@ -78,10 +91,9 @@ int main() {
     cout << "Enter available budget in dollars: \n" << endl;
     cin >> maxBudget;
     MusicFestival festival1;
-    festival1.SetBudget(maxBudget);
+    festival1.setBudget(maxBudget);
 
-
-    /*Musician number1("Travis Scott", 1500000, 29);
+    Musician number1("Travis Scott", 1500000, 29);
     Musician number2("Tory Lanez", 750000, 28);
     Musician number3("A$AP Rocky", 1400000, 32);
     Musician number4("SAINt JHN", 800000, 34);
@@ -90,13 +102,13 @@ int main() {
     Musician number7("Tyler, The Creator", 1350000, 29);
     Musician number8("Denzel Curry", 750000, 25);
 
-    
-
     festival1.addMusician(number1);
     festival1.addMusician(number2);
     festival1.addMusician(number3);
-    festival1.addMusician(number4);*/
-  
+    festival1.addMusician(number4);
+
     cout << "\nAdded " << festival1.getArtists().size() << " artist(s) in all." << endl;
+
+    return 0;
 }
 
